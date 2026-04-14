94096c70ddd2724c71fff5a24ebc9d4d63aa6017
weishenglim04 <limweisheng0714@gmail.com>
2026-04-14 13:41:13 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cxrnc4ptenix.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "BMIT3723"
+customdb = "employee"
+custombucket = "limweisheng-employee"
+customregion = "us-east-1"
+
